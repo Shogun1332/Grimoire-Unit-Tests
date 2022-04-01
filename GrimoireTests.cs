74cf9aae4724bc4fb4810347dab2784138a28060
Dom Tolman <Tolmdomi@gmail.com>
2022-04-01 17:52:03 -0600

    Add project files.

---
+++ GrimoireTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using C969___Scheduling_Software;
+using System;
+
+namespace Grimoire_Unit_Tests
+{
+    [TestClass]
+    public class GrimoireTests
+    {
+        mySQLDB mySQLDB = new mySQLDB();
+        string user = "test";
+        string pass = "test";
+        int expectedUserId = 1;
+        int returnedUserId = 0;
+        Exception exception;
+
+        [TestMethod]
+        public void TestValidLogin() //Will test that when supplied with valid credentials the login window will appropriately provide access to the application
+        {
+            try
+            {
+                returnedUserId = mySQLDB.LoginDBUser(user, pass);
+            }
+            catch (Exception e)
+            {
+                exception = e;
+            }
+            Assert.AreEqual(expectedUserId, returnedUserId); //Credentials supplied map to userId 1 in the database, so we expect the return from the login method to be 1
+            Assert.IsNull(exception); //It should not throw an exception
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Grimoire Unit Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Grimoire Unit Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ce4ad30e-b859-478f-96de-121c81c7ef0f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
